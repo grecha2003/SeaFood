a42d5a313b80a14cf3fe47304709a31d415002e5
David2021 <79111749+grecha2003@users.noreply.github.com>
2021-12-07 21:02:27 +0300

    update

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+
+  $('.header__slider').slick({
+    dots: true,
+    fade: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/prev.svg" alt="prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt="next"></button>',
+  });
+});
